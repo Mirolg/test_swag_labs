7111c171507d8ae86e6702cd2b45429bcb1ce8fb
Mirolg <potrykusmirek@gmail.com>
2022-06-26 15:53:02 +0200

    create fixtures in files : driver, login_page, inventory_page. Add fixtures in files: test_basket_page, test_login_page, test_inventory

---
+++ Pages/drive.py
@@ -0,0 +1,11 @@
+# import pytest
+# from selenium import webdriver
+#
+#
+# @pytest.fixture(params=["Chrome"], scope="class")
+# def init_driver(request):
+#     driver = webdriver.Chrome()
+#     request.cls.driver = driver
+#     yield
+#     driver.close()
+
