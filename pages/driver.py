c92da2064a83136fa2bc1bf48ab005a62ec1252a
Mirolg <potrykusmirek@gmail.com>
2022-07-04 13:18:41 +0200

    change driver.py

--- pages/driver.py
+++ pages/driver.py
@@ -2,7 +2,8 @@ import pytest
 from selenium.webdriver import Chrome
 import chromedriver_autoinstaller
 
-chromedriver_autoinstaller.install()
+
+# chromedriver_autoinstaller.install()
 
 
 @pytest.fixture
